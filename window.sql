136662774ebc9c20e2f8791ea7b57faba681d82d
Pratik Ramteke <148608558+PratikPhysics@users.noreply.github.com>
2025-10-15 12:42:16 +0530

    Update window.sql

--- window.sql
+++ window.sql
@@ -6,31 +6,33 @@ create table employee
 , DEPT_NAME varchar(50)
 , SALARY int);
 
-insert into employee values(101, 'Mohan', 'Admin', 4000);
-insert into employee values(102, 'Rajkumar', 'HR', 3000);
-insert into employee values(103, 'Akbar', 'IT', 4000);
-insert into employee values(104, 'Dorvin', 'Finance', 6500);
-insert into employee values(105, 'Rohit', 'HR', 3000);
-insert into employee values(106, 'Rajesh',  'Finance', 5000);
-insert into employee values(107, 'Preet', 'HR', 7000);
-insert into employee values(108, 'Maryam', 'Admin', 4000);
-insert into employee values(109, 'Sanjay', 'IT', 6500);
-insert into employee values(110, 'Vasudha', 'IT', 7000);
-insert into employee values(111, 'Melinda', 'IT', 8000);
-insert into employee values(112, 'Komal', 'IT', 10000);
-insert into employee values(113, 'Gautham', 'Admin', 2000);
-insert into employee values(114, 'Manisha', 'HR', 3000);
-insert into employee values(115, 'Chandni', 'IT', 4500);
-insert into employee values(116, 'Satya', 'Finance', 6500);
-insert into employee values(117, 'Adarsh', 'HR', 3500);
-insert into employee values(118, 'Tejaswi', 'Finance', 5500);
-insert into employee values(119, 'Cory', 'HR', 8000);
-insert into employee values(120, 'Monica', 'Admin', 5000);
-insert into employee values(121, 'Rosalin', 'IT', 6000);
-insert into employee values(122, 'Ibrahim', 'IT', 8000);
-insert into employee values(123, 'Vikram', 'IT', 8000);
-insert into employee values(124, 'Dheeraj', 'IT', 11000);
-COMMIT;
+INSERT INTO employee (emp_id, emp_name, department, salary)
+VALUES
+(101, 'Mohan', 'Admin', 4000),
+(102, 'Rajkumar', 'HR', 3000),
+(103, 'Akbar', 'IT', 4000),
+(104, 'Dorvin', 'Finance', 6500),
+(105, 'Rohit', 'HR', 3000),
+(106, 'Rajesh', 'Finance', 5000),
+(107, 'Preet', 'HR', 7000),
+(108, 'Maryam', 'Admin', 4000),
+(109, 'Sanjay', 'IT', 6500),
+(110, 'Vasudha', 'IT', 7000),
+(111, 'Melinda', 'IT', 8000),
+(112, 'Komal', 'IT', 10000),
+(113, 'Gautham', 'Admin', 2000),
+(114, 'Manisha', 'HR', 3000),
+(115, 'Chandni', 'IT', 4500),
+(116, 'Satya', 'Finance', 6500),
+(117, 'Adarsh', 'HR', 3500),
+(118, 'Tejaswi', 'Finance', 5500),
+(119, 'Cory', 'HR', 8000),
+(120, 'Monica', 'Admin', 5000),
+(121, 'Rosalin', 'IT', 6000),
+(122, 'Ibrahim', 'IT', 8000),
+(123, 'Vikram', 'IT', 8000),
+(124, 'Dheeraj', 'IT', 11000);
+
 
 
 /* **************
